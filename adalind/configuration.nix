2ef143a8db2573f1f65770a9bd8199621cf236f5
Michishige Kaito <me@mkaito.com>
2018-04-09 22:39:22 +0100

    ZNC config

--- adalind/configuration.nix
+++ adalind/configuration.nix
@@ -14,4 +14,37 @@
     rsyncKeys = builtins.concatLists (builtins.attrValues (import <mkaito/keys/ssh.nix>));
     autoStart = false;
   };
+
+  ## IRC Bouncer
+  services.znc = {
+    enable = true;
+    mutable = false;
+    openFirewall = true;
+    confOptions = {
+      userName = "chris";
+      nick = "mkaito";
+      passBlock = ''
+        <Pass password>
+                Method = sha256
+                Hash = 2b90c7083ead25fc0dc579e271aadcf5e602794681628668cfaa9edbdaecd378
+                Salt = i,XnYtW*nTR(A!j;W_!a
+        </Pass>
+      '';
+      networks = {
+        freenode = {
+          channels = [ "#starcraft" "nixos" "#linux" "afewmail" "ruby" "weechat" ];
+          server = "irc.freenode.net";
+          port = 7000;
+          modules = [ "chansaver" "keepnick" "nickserv TDHHbEvoA8efYveD" "route_replies" ];
+          extraConf = ''
+            FloodBurst = 4
+            FloodRate = 1.00
+            IRCConnectEnabled = true
+            Ident = kaito
+            RealName = kaito
+          '';
+        };
+      };
+    };
+  };
 }

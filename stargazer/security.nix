6edbe076daca89578e3e9f3f7464c48ddecc06aa
Christian Höppner <chris@mkaito.net>
2020-11-21 11:54:08 +0000

    Add myself to trusted-users, add builder SSH key

--- stargazer/security.nix
+++ stargazer/security.nix
@@ -23,15 +23,21 @@ in {
   };
 
   security.sudo.wheelNeedsPassword = false;
+  nix.trustedUsers = [ "root" "@wheel" ];
 
   users.mutableUsers = false;
   users.users = lib.recursiveUpdate (lib.mapAttrs expandUser sshKeys) {
     root = {
       hashedPassword = "$6$lTBGqUqKYw$sBQXsEfL5FqwYbJlyejWRoagNUjoALM6VCtz7qI6veS.lIluw9cPx8NDmoinWFzS.g8WBuZCQZxs8NTmns/G4/";
       openssh.authorizedKeys.keys = sshKeys.chris ++
-        # Deployment key used on builds.sr.ht
-        #   secret: 04e2a5e7-5c88-45ad-a806-c5d0073343dc
-        ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGdwmaXyjrewrD5Bc6zpEJfzi38FDR5kqUI2rqKNcG6"];
+        [
+          # Deployment key used on builds.sr.ht
+          #   secret: 04e2a5e7-5c88-45ad-a806-c5d0073343dc
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGdwmaXyjrewrD5Bc6zpEJfzi38FDR5kqUI2rqKNcG6"
+
+          # Remote builds
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKQu6N8OM7hU105dnLpfeRJqpglaoD515pUhwDafFHpK root@cryptbreaker"
+        ];
     };
 
     chris.hashedPassword = "$6$5cT0x8HjQq$CmQt274.cqvOlJM/9M1qTBSlcH19G8iaxHNkFRqMZAUtuhHjDGSkfqb5LEd2C7fQtLpXnUSQWYcZu3qsbRJZr.";

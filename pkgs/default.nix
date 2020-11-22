ca27bbfa754d4ecf71d9b540fe91e8990dda5d9f
Christian Höppner <chris@mkaito.net>
2020-11-22 21:11:39 +0000

    Delete old obsolete things

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,8 +7,6 @@ let
 in rec {
   nixUnstable = inputs.nix.defaultPackage.${final.system};
 
-  factorio = callPackage ./factorio { releaseType = "alpha"; };
-  factorio-experimental = factorio.override { releaseType = "alpha"; experimental = true; };
   factorio-headless = factorio.override { releaseType = "headless"; };
   factorio-headless-experimental = factorio.override { releaseType = "headless"; experimental = true; };
 }

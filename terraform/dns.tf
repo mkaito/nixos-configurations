8f53f8bab32b96ddd11335f9975d7211cc2f475b
Christian Höppner <chris@mkaito.net>
2021-02-08 01:05:51 +0000

    Deploy VM DNS entries

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -11,3 +11,22 @@ data "dns_a_record_set" "stargazer" {
 data "dns_aaaa_record_set" "stargazer" {
   host = "stargazer.mkaito.net"
 }
+
+# Public VM IPs
+resource "aws_route53_record" "vmx_stargazer_mkaito_net_ipv4" {
+  for_each = toset( ["69", "70", "71", "72", "73", "74"])
+  zone_id = data.aws_route53_zone.mkaito_net.zone_id
+  name    = "vm${each.key}.stargazer.${data.aws_route53_zone.mkaito_net.name}"
+  type    = "A"
+  ttl     = "60"
+  records = ["95.217.103.1${each.key}"]
+}
+
+resource "aws_route53_record" "vmx_stargazer_mkaito_net_ipv6" {
+  for_each = toset( ["69", "70", "71", "72", "73", "74"])
+  zone_id = data.aws_route53_zone.mkaito_net.zone_id
+  name    = "vm${each.key}.stargazer.${data.aws_route53_zone.mkaito_net.name}"
+  type    = "AAAA"
+  ttl     = "60"
+  records = ["2a01:4f9:4b:12e2::${each.key}"]
+}
